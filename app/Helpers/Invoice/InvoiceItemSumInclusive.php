dd4c1f7c90ab70a3b83da87fb65a477c53a1bb74
Mathis Scholz <34720184+MathisScholz@users.noreply.github.com>
2026-04-07 16:44:16 +0200

    feat: multiply line total by rental_days in InvoiceItemSumInclusive

--- app/Helpers/Invoice/InvoiceItemSumInclusive.php
+++ app/Helpers/Invoice/InvoiceItemSumInclusive.php
@@ -174,7 +174,8 @@ class InvoiceItemSumInclusive
 
     private function sumLineItem()
     {
-        $this->setLineTotal($this->item->cost * $this->item->quantity);
+        $rentalDays = isset($this->item->rental_days) && $this->item->rental_days > 0 ? $this->item->rental_days : 1;
+        $this->setLineTotal($this->item->cost * $this->item->quantity * $rentalDays);
 
         return $this;
     }
